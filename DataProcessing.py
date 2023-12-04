579a00d08433bf696774377f4cbc70131bb269e9
sagarswapRIT <sagarswap@gmail.com>
2023-12-03 19:03:33 -0500

    Data Provessing Implemnted. 4 models have been added

--- DataProcessing.py
+++ DataProcessing.py
@@ -1,4 +1,14 @@
 import pandas as pd
+import numpy as np
 
 content=pd.read_csv('./data/cyberml_-_cyberml.csv')
-print(content.head(5))
+for (columnName, columnData) in content.iteritems():
+    content[columnName]=content[columnName].apply(hash)
+# content.drop('_source.timestamp', axis=1)
+# content=content.apply(hash)
+# print(content)
+#print(content.cov())
+lessContent=content[['_source.rule.firedtimes', '_source.rule.id', '_source.id', '_source.syscheck.size_after', '_source.syscheck.inode_after', '_source.syscheck.size_before', '_source.syscheck.inode_before', 'malicious']]
+print(content)
+content.to_csv('./data/processedData.csv', index=False)
+lessContent.to_csv('./data/processedLessData.csv', index=False)
